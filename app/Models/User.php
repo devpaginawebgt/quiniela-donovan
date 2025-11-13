f342ed7b284352dffaf96eca9663f2834fe15d12
Dennis Herrera <dennishumher25@hotmail.com>
2025-11-13 10:18:00 -0600

    feat(users): added user push tokens relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -57,5 +58,10 @@ class User extends Authenticatable
     public function codigo() : BelongsTo
     {
         return $this->belongsTo(Codigo::class, 'codigo_id');
+    }
+
+    public function pushTokens(): HasMany
+    {
+        return $this->hasMany(UserPushToken::class);
     }
 }
