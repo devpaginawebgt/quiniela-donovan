6941746333acf96f679d5b1388e552bf75156d9b
Dennis Herrera <dennishumher25@hotmail.com>
2026-02-19 16:23:00 -0600

    chore: added blue style to groups view

--- resources/views/modulos/grupos.blade.php
+++ resources/views/modulos/grupos.blade.php
@@ -6,16 +6,16 @@
     </x-slot>
 
     <div class="max-w-screen-2xl my-6 mx-auto sm:px-6 lg:px-8" id="selecciones-container">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="px-6 pb-6 bg-white border-b border-gray-200 ">
+        <div class="overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="px-6 pb-6 mb-8">
                 <h5 class="text-3xl text-center font-bold my-8">Grupos conformados</h5>
                 <div class="flex flex-col">
                     <div class="xl:w-1/6 w-1/2 mx-auto mb-4">
-                        <label for="grupos" class="block mb-2 text-sm font-medium text-gray-900">Seleccione: </label>
+                        <label for="grupos" class="block mb-2 text-sm font-medium text-[--light-color] text-center">Seleccione: </label>
                         <div class="flex items-center justify-center">
                             <select
                                 id="grupos"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-lg font-semibold text-center cursor-pointer rounded-lg focus:ring-red-800 focus:border-red-800 block w-1/2 p-2.5"
+                                class="bg-[--complementary-primary-color] border border-[--complementary-light-color] text-[--light-color] font-semibold text-center cursor-pointer rounded-lg block w-1/2 p-2.5"
                                 {{-- onchange="verEquiposGrupo(this)" --}}
                             >
                                 @foreach($grupos as $grupo)
@@ -29,9 +29,10 @@
                             <svg class="animate-spin spinner-load" viewBox="0 0 24 24"></svg>
                         </div>
                     </div>
-                    <div class="w-full lg:w-3/4 overflow-x-auto relative shadow-md sm:rounded-lg mx-auto">
-                        <table class="xl:w-full text-sm text-left text-gray-500">
-                            <thead class="text-xs text-gray-100 uppercase" style="background-color: #000">
+
+                    <div class="w-full lg:w-3/4 overflow-x-auto relative shadow-md sm:rounded-lg mx-auto mb-12">
+                        <table class="xl:w-full text-sm text-left">
+                            <thead class="text-xs bg-[--dark-color] uppercase">
                                 <tr>
                                     <th scope="col" class="py-3 px-6">Equipo</th>
                                     <th scope="col" class="py-3 px-6">PJ</th>
@@ -46,12 +47,12 @@
                             <tbody id="body-equipos-grupo">
                                 @if (!empty($equipos_grupo))
                                     @foreach($equipos_grupo as $equipo)
-                                        <tr class="bg-white border-b">
-                                            <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap flex items-center justify-between">
+                                        <tr class="bg-[--complementary-primary-color] border-b border-zinc-400 text-[--light-color]">
+                                            <th scope="row" class="py-4 px-6 font-medium whitespace-nowrap flex items-center justify-between">
                                                 <img
                                                     src="{{ $equipo->imagen }}"
                                                     alt="SELECCION"
-                                                    class="h-10 w-14 mx-4 border rounded-md shadow-md"
+                                                    class="h-10 w-14 mx-4 rounded-md shadow-md"
                                                 >
                                                 {{ $equipo->nombre }}
 
@@ -84,29 +85,31 @@
                         </table>
                     </div>
 
-                    <div class="shadow-md rounded-md mx-auto w-full lg:w-3/4 my-4">
-                        <p class="p-4 border-b text-xl" style="border-color: #177245;">
-                            Jornada 1
-                        </p>
+                    <p class="text-xl font-semibold text-center mb-4">
+                        Jornada 1
+                    </p>
 
+                    <div class="shadow-md mx-auto w-full lg:w-3/4 bg-[--complementary-primary-color] rounded-2xl overflow-hidden p-4 mb-12">
                         <ul id="partidos-jornada-1">
 
                             @if(!empty($jornada_uno))
 
-                                @foreach($jornada_uno['partidos'] as $equipos_partido)
+                                @foreach($jornada_uno['partidos'] as $index => $equipos_partido)
 
                                     @php
                                         $fecha_utc = $equipos_partido->partido->fecha_partido;
                                         $timezone = auth()->user()->country->timezone;
 
-                                        $fecha_local = $fecha_utc->copy()->timezone($timezone);
+                                        $fecha_local = $fecha_utc
+                                            ->copy()
+                                            ->timezone($timezone)
+                                            ->locale('es');
 
                                         $fecha_partido = $fecha_local->isoFormat('dddd, D [de] MMMM [de] YYYY');
-                                        $hora_partido  = $fecha_local->format('H:i A');
-
+                                        $hora_partido  = $fecha_local->translatedFormat('H:i A');
                                     @endphp
                                 
-                                    <li class="flex justify-around py-6 lg:py-4 border-b border-gray-300 items-center">
+                                    <li class="flex justify-around py-6 lg:py-4 border-b border-zinc-400 items-center mb-4">
 
                                         <div class="w-1/2 flex-col lg:flex-row xl:w-1/4 flex items-center justify-between">
 
@@ -142,10 +145,11 @@
                         </ul>
                     </div>
 
-                    <div class="shadow-md rounded-md mx-auto w-full lg:w-3/4 my-4">
-                        <p class="p-4 border-b text-xl" style="border-color: #177245;">
-                            Jornada 2
-                        </p>
+                    <p class="text-xl font-semibold text-center mb-4">
+                        Jornada 2
+                    </p>
+
+                    <div class="shadow-md mx-auto w-full lg:w-3/4 bg-[--complementary-primary-color] rounded-2xl overflow-hidden p-4 mb-12">
                         <ul id="partidos-jornada-2">
 
                             @if(!empty($jornada_dos))
@@ -156,14 +160,17 @@
                                         $fecha_utc = $equipos_partido->partido->fecha_partido;
                                         $timezone = auth()->user()->country->timezone;
 
-                                        $fecha_local = $fecha_utc->copy()->timezone($timezone);
+                                        $fecha_local = $fecha_utc
+                                            ->copy()
+                                            ->timezone($timezone)
+                                            ->locale('es');
 
                                         $fecha_partido = $fecha_local->isoFormat('dddd, D [de] MMMM [de] YYYY');
                                         $hora_partido  = $fecha_local->format('H:i A');
 
                                     @endphp
                                 
-                                    <li class="flex justify-around py-6 lg:py-4 border-b border-gray-300 items-center">
+                                    <li class="flex justify-around py-6 lg:py-4 border-b border-zinc-400 items-center mb-4">
 
                                         <div class="w-1/2 flex-col lg:flex-row xl:w-1/4 flex items-center justify-between">
 
@@ -198,10 +205,11 @@
                         </ul>   
                     </div>
 
-                    <div class="shadow-md rounded-md mx-auto w-full lg:w-3/4 my-4">
-                        <p class="p-4 border-b text-xl" style="border-color: #177245;">
-                            Jornada 3
-                        </p>
+                    <p class="text-xl font-semibold text-center mb-4">
+                        Jornada 3
+                    </p>
+
+                    <div class="shadow-md mx-auto w-full lg:w-3/4 bg-[--complementary-primary-color] rounded-2xl overflow-hidden p-4">
                         <ul id="partidos-jornada-3">
 
                             @if(!empty($jornada_tres))
@@ -212,14 +220,17 @@
                                         $fecha_utc = $equipos_partido->partido->fecha_partido;
                                         $timezone = auth()->user()->country->timezone ?? 'GMT-6';
 
-                                        $fecha_local = $fecha_utc->copy()->timezone($timezone);
+                                        $fecha_local = $fecha_utc
+                                            ->copy()
+                                            ->timezone($timezone)
+                                            ->locale('es');
 
                                         $fecha_partido = $fecha_local->isoFormat('dddd, D [de] MMMM [de] YYYY');
                                         $hora_partido  = $fecha_local->format('H:i A');
 
                                     @endphp
                                 
-                                    <li class="flex justify-around py-6 lg:py-4 border-b border-gray-300 items-center">
+                                    <li class="flex justify-around py-6 lg:py-4 border-b border-zinc-400 items-center mb-4">
 
                                         <div class="w-1/2 flex-col lg:flex-row xl:w-1/4 flex items-center justify-between">
 
